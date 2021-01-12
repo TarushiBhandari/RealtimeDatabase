8859b2595095e1679db632e477bbc9efe629b621
TarushiBhandari <lalitbhandari@gmail.com>
2021-01-12 20:59:30 +0530

    Tarushi- learned about realtime databases

--- sketch.js
+++ sketch.js
@@ -1,9 +1,30 @@
 var ball;
+var database;
+var position;
 
 function setup(){
     createCanvas(500,500);
+    database= firebase.database();
+
+    var databaseNodeRef= database.ref('ball/position');
+    databaseNodeRef.on("value",readPosition);
+    
     ball = createSprite(250,250,10,10);
     ball.shapeColor = "red";
+}
+
+function readPosition(data){
+    position= data.val();
+    ball.x= position.x;
+    ball.y= position.y;
+}
+
+function showError(){
+    console.log("there was an error while reading the data");
+}
+
+function writePosition(){
+
 }
 
 function draw(){
