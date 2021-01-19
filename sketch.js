0af977187e9e8a36e524ba05b7e11651901c2283
TarushiBhandari <72614463+TarushiBhandari@users.noreply.github.com>
2021-01-19 11:14:59 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -7,7 +7,7 @@ function setup(){
     database= firebase.database();
 
     var databaseNodeRef= database.ref('ball/position');
-    databaseNodeRef.on("value",readPosition);
+    databaseNodeRef.on("value",readPosition,showError);
     
     ball = createSprite(250,250,10,10);
     ball.shapeColor = "red";
@@ -23,25 +23,33 @@ function showError(){
     console.log("there was an error while reading the data");
 }
 
-function writePosition(){
-
+function writePosition(x,y){
+    database.ref('ball/position').set({
+        'x': position.x+x,
+        'y': position.y+y
+    })
 }
 
 function draw(){
     background("white");
-    if(keyDown(LEFT_ARROW)){
-        changePosition(-1,0);
-    }
-    else if(keyDown(RIGHT_ARROW)){
-        changePosition(1,0);
-    }
-    else if(keyDown(UP_ARROW)){
-        changePosition(0,-1);
-    }
-    else if(keyDown(DOWN_ARROW)){
-        changePosition(0,+1);
-    }
+
+    if(position!==undefined){
+        if(keyDown(LEFT_ARROW)){
+            writePosition(-1,0);
+        }
+        else if(keyDown(RIGHT_ARROW)){
+            writePosition(1,0);
+        }
+        else if(keyDown(UP_ARROW)){
+            writePosition(0,-1);
+        }
+        else if(keyDown(DOWN_ARROW)){
+            writePosition(0,+1);
+        }
+         
     drawSprites();
+    }
+   
 }
 
 function changePosition(x,y){
